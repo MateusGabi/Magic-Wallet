9445751829356b221b6a1cdb70d631f4fb41cc7f
Mateus Gabi Moreira <mateusgabimoreira@gmail.com>
2020-05-21 15:00:42 -0300

    added wrapper to yahoo finance api

--- stocks-service/src/main/java/net/mateusgabi/magicwallet/stocksservice/resources/StockResource.java
+++ stocks-service/src/main/java/net/mateusgabi/magicwallet/stocksservice/resources/StockResource.java
@@ -1,16 +1,23 @@
 package net.mateusgabi.magicwallet.stocksservice.resources;
 
-import net.mateusgabi.magicwallet.stocksservice.models.Stock;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/stocks")
 public class StockResource {
 
     @RequestMapping("/{ticker}")
-    public Stock getStockDetails(@PathVariable("ticker") String ticker) {
-        return new Stock(ticker, "My company foo");
+    public Stock getStockDetails(@PathVariable("ticker") String ticker) throws IOException {
+        Stock yahooFinanceStock = YahooFinance.get(ticker);
+
+        BigDecimal price = yahooFinanceStock.getQuote().getPrice();
+        return yahooFinanceStock;
     }
 }
